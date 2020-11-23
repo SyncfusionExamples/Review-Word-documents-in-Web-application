6a5239704db5b90be350bd1245ffcfa83bb0d871
gunasekarant <gunasekarant@syncfusion.com>
2020-11-23 21:24:19 +0530

    Added server-side examples for accept or reject changes using Syncfusion Word library.

---
+++ Server-side/Accept-all-changes-made-by-an-author/Console-App-.NET-Core/Program.cs
@@ -0,0 +1,53 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reads the input Word document
+            Stream inputStream = File.OpenRead(Path.GetFullPath(@"../../../../../../Data/InputWithTrackedChanges.docx"));
+            string authorName = "Steven Buchanan";
+            int acceptedChangesCount = AcceptChanges(inputStream, authorName);
+            Console.WriteLine("Accepted {0} changes made by {1}.", acceptedChangesCount, authorName);
+        }
+        /// <summary>
+        ///  Accepts all the changes made by an author.
+        /// </summary>
+        static int AcceptChanges(Stream inputStream, string authorName)
+        {
+            int acceptedChangesCount = 0;
+            //Creates new Word document instance for Word processing
+            using (WordDocument document = new WordDocument())
+            {
+                //Opens the Word document containing tracked changes
+                document.Open(inputStream, FormatType.Docx);
+                inputStream.Dispose();
+                //Iterates all the revisions present in the Word document
+                for (int i = document.Revisions.Count - 1; i >= 0; i--)
+                {
+                    //Validates the author of current revision to accepts/rejects it
+                    if (document.Revisions[i].Author == authorName)
+                    {
+                        //Accepts the current revision
+                        document.Revisions[i].Accept();
+                        acceptedChangesCount++;
+                    }
+                    //Resets i to last item, since accepting one revision will impact all its related revisions and leads to change in Revsions
+                    if (i > document.Revisions.Count - 1)
+                        i = document.Revisions.Count;
+                }
+                //Saves the Word document as DOCX format
+                using (Stream docStream = File.Create(Path.GetFullPath(@"../../../OutputAfterAcceptingChanges.docx")))
+                {
+                    document.Save(docStream, FormatType.Docx);
+                }
+            }
+            return acceptedChangesCount;
+        }
+    }
+}
---
+++ Server-side/Reject-insert-delete-revisions-made-by-all-authors/Console-App-.NET-Core/Program.cs
@@ -0,0 +1,53 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reads the input Word document
+            Stream inputStream = File.OpenRead(Path.GetFullPath(@"../../../../../../Data/InputWithTrackedChanges.docx"));
+            RevisionType revisionType = RevisionType.Insertions | RevisionType.Deletions;
+            int rejectedChangesCount = RejectChanges(inputStream, revisionType);
+            Console.WriteLine("Rejected {0} revisions for {1} made by all authors.", rejectedChangesCount, revisionType);
+        }
+        /// <summary>
+        ///  Rejects the insert and delete revisions made by all authors.
+        /// </summary>
+        static int RejectChanges(Stream inputStream, RevisionType revisionType)
+        {
+            int rejectedChangesCount = 0;
+            //Creates new Word document instance for Word processing
+            using (WordDocument document = new WordDocument())
+            {
+                //Opens the Word document containing tracked changes
+                document.Open(inputStream, FormatType.Docx);
+                inputStream.Dispose();
+                //Iterates all the revisions present in the Word document
+                for (int i = document.Revisions.Count - 1; i >= 0; i--)
+                {
+                    //Validates the revision type of current revision to accepts/rejects it
+                    if ((document.Revisions[i].RevisionType & revisionType) != 0)
+                    {
+                        //Rejects the current revision
+                        document.Revisions[i].Reject();
+                        rejectedChangesCount++;
+                    }
+                    //Resets i to last item, since rejecting one revision will impact all its related revisions and lead to change in Revsions
+                    if (i > document.Revisions.Count - 1)
+                        i = document.Revisions.Count;
+                }
+                //Saves the Word document as DOCX format
+                using (Stream docStream = File.Create(Path.GetFullPath(@"../../../OutputAfterRejectingChanges.docx")))
+                {
+                    document.Save(docStream, FormatType.Docx);
+                }
+            }
+            return rejectedChangesCount;
+        }
+    }
+}
