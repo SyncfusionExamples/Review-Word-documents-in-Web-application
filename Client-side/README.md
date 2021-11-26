d5e01747d65de23fe405dba50179a185faba0daa
Suriya.Murugan <suriya.murugan@syncfusion.com>
2021-11-26 14:00:59 +0530

    Updated documenteditor getting started sample

---
+++ Client-side/README.md
@@ -0,0 +1,17 @@
+# Getting started with Javascript Syncfusion Document editor
+
+Above sample is written in Javascript. You can also write the client-side sample in  [`Angular`](https://github.com/SyncfusionExamples/View-and-edit-Word-document-in-Angular), [`React`](https://github.com/SyncfusionExamples/View-and-edit-Word-document-in-React) and [`vue`](https://github.com/SyncfusionExamples/View-and-edit-Word-document-in-Vue).
+
+## To run the sample
+
+You can run the below command to install all dependent packages related to this seed project.
+
+```
+npm install
+```
+
+You can use the below npm script to run the web application.
+
+```
+npm start
+```
---
+++ Client-side/src/app/app.ts
@@ -0,0 +1,42 @@
+
+import { DocumentEditorContainer, Toolbar } from '@syncfusion/ej2-documenteditor';
+import { TitleBar } from './title-bar';
+
+/**
+ * Default document editor sample
+ */
+
+    
+    let hostUrl: string = 'https://ej2services.syncfusion.com/production/web-services/';
+
+    let container: DocumentEditorContainer = new DocumentEditorContainer({ enableToolbar: true });
+    DocumentEditorContainer.Inject(Toolbar);
+    container.serviceUrl = hostUrl + 'api/documenteditor/';
+    container.created = (): void => {
+        setInterval(() => {
+          updateDocumentEditorSize();
+        }, 100);
+        //Adds event listener for browser window resize event.
+        window.addEventListener('resize', onWindowResize);
+      };
+    container.appendTo('#container');
+    let titleBar: TitleBar = new TitleBar(document.getElementById('documenteditor_titlebar'), container.documentEditor, true);
+    container.documentEditor.open(JSON.stringify((titleBar.data)));
+    container.documentEditor.documentName = 'Getting Started';
+    titleBar.updateDocumentTitle();
+   
+    container.documentChange = (): void => {
+        titleBar.updateDocumentTitle();
+        container.documentEditor.focusIn();
+    };
+
+    function onWindowResize() {
+        //Resizes the document editor component to fit full browser window automatically whenever the browser resized.
+        updateDocumentEditorSize();
+      }
+      function updateDocumentEditorSize() {
+        //Resizes the document editor component to fit full browser window.
+        var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight - titleBar.getHeight();
+        container.resize(windowWidth, windowHeight);
+      }
